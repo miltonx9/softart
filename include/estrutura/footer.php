dce7c0b04c95f33be1211588a88df991773afd5d
Alex Rodrigues <alexcleiton16@gmail.com>
2017-09-19 07:08:55 -0300

    Arquivos desnecessarios

--- include/estrutura/footer.php
+++
@@ -1,9 +0,0 @@
-<footer style="text-align:center" class="bd-footer text-muted">
-  <div class="container-fluid p-3 p-md-5">
-    
-      
-     <a href="#">JJMUNDO</a> 
-    <p>Todos Direitos Reservados <a href="#"</a></p>
-    <p>FEJJA - 2018<a href="#"</a></p>
-  </div>
-</footer>
